1857ef5ed55c3a81bb164a395b4aebd98947ca3b
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-09-02 10:49:24 +0530

    [Vaibhav] added ability to analyze & respond mood test

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyze.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyze.java
@@ -2,5 +2,13 @@ package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyze 
 {
-	
+	public String analyseMood(String message)
+	{
+        if (message.toLowerCase().contains("happy"))
+            return "happy";
+        else if (message.toLowerCase().contains("sad"))
+            return "sad";
+        else
+            return "happy";
+    }
 }
--- src/test/java/com/bridgelabs/moodanalyzertest/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabs/moodanalyzertest/MoodAnalyzerTest.java
@@ -7,5 +7,12 @@ import com.bridgelabz.moodanalyzer.MoodAnalyze;
 
 public class MoodAnalyzerTest 
 {
-	
+	MoodAnalyze moodAnalyzer = new MoodAnalyze();
+
+    @Test
+    public void respondMood_Sad_Or_Happpy()
+    {
+        String result = moodAnalyzer.analyseMood("I am happy");
+        Assert.assertEquals("happy", result);
+    }
 }
