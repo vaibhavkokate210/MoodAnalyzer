e581efc68c16e07fe9430382824f44b713f811d4
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-09-02 10:47:44 +0530

    [Vaibhav] added inital commit

---
+++ src/test/java/com/bridgelabs/moodanalyzertest/MoodAnalyzerTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabs.moodanalyzertest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.moodanalyzer.MoodAnalyze;
+
+public class MoodAnalyzerTest 
+{
+	
+}
